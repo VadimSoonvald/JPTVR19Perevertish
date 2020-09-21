f65e6d9869a244fdf807c7aa0ced8d0bf0dbf145
pupil <pupil@ivkhk.ee>
2020-09-21 10:40:11 +0300

    Готовый проект

--- src/jptvr19perevertish/App.java
+++ src/jptvr19perevertish/App.java
@@ -5,6 +5,8 @@
  */
 package jptvr19perevertish;
 
+import java.util.Scanner;
+
 /**
  *
  * @author pupil
@@ -12,7 +14,30 @@ package jptvr19perevertish;
 class App {
     public void run(){
         System.out.println("--- Перевёртыш ---");
-    
+    Scanner scanner = new Scanner (System.in);
+        System.out.print("Введите строку символов: ");
+        String str = scanner.nextLine();
+        System.out.println("В этой строке символов: "+str.length());
+        char[] chStr = str.toCharArray();
+        char[] reversStr = new char[chStr.length];
         
+        for (int i = 0; i < reversStr.length; i++) {
+            reversStr[i] = chStr[reversStr.length - 1 - i];
+            
+        }
+        String rStr = reversStr.toString();
+        System.out.print("Обратная строка: "+ new String(reversStr));
+        System.out.println(" ");
+        System.out.print("Второе слово в строке: ");
+        int firstSpace = str.indexOf(" ");
+        String str2 = str.substring(firstSpace + 1);
+        int nextSpace = str2.indexOf(" ");
+        String secondWord;
+        if(nextSpace > 0){
+            secondWord = str.substring(firstSpace + 1, nextSpace + firstSpace + 1);
+        }else{
+            secondWord = str2;
+        }
+        System.out.println(secondWord);
     }
 }
